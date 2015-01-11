fa4b8daab92feff8fcfa81e087ebb02d51131e17
dekokun <shintaro.kurachi@gmail.com>
2015-01-11 18:30:13 +0900

    Revert "リセット機構は役に立っているのか" よくわからん…
    This reverts commit e39568c0c77e1c62e88bbe0a4731ba097fa159af.

--- lib/Game.php
+++ lib/Game.php
@@ -42,7 +42,7 @@ class Game {
             // 6ターン目から一度情報が新しくなるためデート回数はリセットしておく
             if ($this->turn->getNextTurn() === 6) {
                 record('resetted');
-//                $this->lords->resetEstimatedNegotiationCount();
+                $this->lords->resetEstimatedNegotiationCount();
             }
             $lords
                 = $this->ruleSelector
